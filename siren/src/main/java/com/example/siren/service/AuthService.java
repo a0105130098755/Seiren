5e7e0c9802aa5fef44b8f36385ae7988ac5219df
qarksangdol <0629psw@gmail.com>
2024-06-20 16:38:27 +0900

    메일인증 Service 구현

--- siren/src/main/java/com/example/siren/service/AuthService.java
+++ siren/src/main/java/com/example/siren/service/AuthService.java
@@ -3,9 +3,16 @@ package com.example.siren.service;
 import com.example.siren.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.transaction.Transactional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +20,8 @@ import javax.transaction.Transactional;
 @Slf4j
 public class AuthService {
     private final MemberRepository memberRepository;
+    private final JavaMailSender mailSender;
+    private final MimeMessage mimeMessage = mailSender.createMimeMessage();
 
     public boolean existsEmail(String email){
         try{
@@ -34,5 +43,33 @@ public class AuthService {
         }
     }
 
+    //메일 발송 뒤 해당 코드 반환
+    public String sendMail(String email){
+        Random random = new Random();
+        int min = 111111;
+        int max = 999999;
+        String code = String.valueOf(random.nextInt(max - min + 1) + min);
+        log.info("인증 번호 : " + code);
+
+        String htmlContent = "<div style=\"text-align: center; display:flex; flex-direction:column; justify-content:center; text-align:center;\">"
+                + "<p style=\"font-size:30px; display: block;\">Siren 인증번호 입니다.</p>"
+                + "<p></p>"
+                + "<p style=\"font-size:16px; display: block;\">아래의 인증 번호를 입력해주세요.</p>"
+                + "<p></p>"
+                + "<div style=\"font-size:20px; font-style:bold; width: 100%; height:50px; border: 1px solid #c6c6c6; display: block;\">" + code + "</div>"
+                + "</div>";
+
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+        try {
+            helper.setFrom("1103bsj@naver.com");
+            helper.setTo(email);
+            helper.setSubject("AskMe 이메일 인증 번호");
+            helper.setText(htmlContent, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        mailSender.send(mimeMessage);
+        return code;
+    }
 
 }
