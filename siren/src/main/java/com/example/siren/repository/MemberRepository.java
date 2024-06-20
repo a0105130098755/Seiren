0e1f8994cca09daa035c37e1115faf03e6654344
qarksangdol <0629psw@gmail.com>
2024-06-20 15:04:01 +0900

    MemberRepository 일부 구성

--- siren/src/main/java/com/example/siren/repository/MemberRepository.java
+++ siren/src/main/java/com/example/siren/repository/MemberRepository.java
@@ -1,4 +1,14 @@
 package com.example.siren.repository;
 
-public interface MemberRepository {
+import com.example.siren.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    // 회원 정보에 관련된 DB 작업들
+    // 회원가입 ( 이메일 존재 여부, 닉네임 존재 여부, 회원 가입 데이터 저장)
+    boolean existByEmail(String email);
+    boolean existByNickname(String email);
+
 }
