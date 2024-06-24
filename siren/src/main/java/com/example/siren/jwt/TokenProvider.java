babb3baefe57540b940a72f2c56cf87a4f456b09
qarksangdol <0629psw@gmail.com>
2024-06-24 11:54:07 +0900

    TokenProvider, refreshToken 및 주석 추가

--- siren/src/main/java/com/example/siren/jwt/TokenProvider.java
+++ siren/src/main/java/com/example/siren/jwt/TokenProvider.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.security.core.Authentication;
-
 import java.security.Key;
 import java.util.Arrays;
 import java.util.Collection;
@@ -25,18 +24,21 @@ import java.util.stream.Collectors;
 public class TokenProvider {
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; // 1000 = 1초
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000* 60 * 60 * 24 * 7L; // 7일
     private final Key key;
 
     // 주의점 : @Value 어노테이션은 spring framework 의 어노테이션이다.
+    // 생성자 : secretKey 매개변수는 @Value 어노테이션으로 주입, JWT 만들 때 사용하는 암호화 키 생성
     public TokenProvider(@Value("${jwt.secret}") String secretKey) {
         //this.key = Keys.secretKeyFor(SignatureAlgorithm.HS512); // HS512 알고리즘을 사용하는 키 생성
         this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
-    // 토큰 생성
+    // 토큰 생성을 담당하는 메소드
+    // Authentication 객체 기반 JWT 토큰 생성
     public TokenDTO generateTokenDto(Authentication authentication) {
-
+        // 권한 정보 문자열 생성
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
@@ -44,9 +46,13 @@ public class TokenProvider {
         long now = (new Date()).getTime();
 
 
-        Date tokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        Date tokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME); // accessToken 만료 시간
+        Date refreshTokenExpiresIn = new Date(now + REFRESH_TOKEN_EXPIRE_TIME); // refreshToken 만료 시간
 
-        System.out.println(tokenExpiresIn);
+        log.info("TokenProvider getName {}",authentication.getName());
+        log.info("TokenProvider getPrincipal {}",authentication.getDetails());
+        log.info("TokenProvider getAuthorities {}",authentication.getAuthorities());
+
 
         String accessToken = Jwts.builder()
                 .setSubject(authentication.getName())
@@ -55,30 +61,48 @@ public class TokenProvider {
                 .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
 
+        String refreshToken =Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .setExpiration(refreshTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+        log.info("Refresh Token : {}", refreshToken);
+
         return TokenDTO.builder()
                 .grantType(BEARER_TYPE)
                 .accessToken(accessToken)
                 .tokenExpiresIn(tokenExpiresIn.getTime())
+                .refreshToken(refreshToken)
+                .refreshTokenExpiresIn(refreshTokenExpiresIn.getTime())
                 .build();
     }
 
-    public Authentication getAuthentication(String accessToken) {
+    // 토큰 값 받아서 복호화 후 사용자에 대한 정보 반환 메소드
+    public Authentication getAuthentication(String accessToken){
+        // 토큰 복호화
         Claims claims = parseClaims(accessToken);
 
+        //보호화에 실패하면
         if (claims.get(AUTHORITIES_KEY) == null) {
             throw new RuntimeException("권한 정보가 없는 토큰입니다.");
         }
 
+        // 토큰에 담긴 권한 정보들을 가져옴
         Collection<? extends GrantedAuthority> authorities =
                 Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
                         .map(SimpleGrantedAuthority::new)
                         .collect(Collectors.toList());
 
-        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        // 권한 정보들을 이용해 유저 객체를 만들어서 반환, 여기서 User 객체는 UserDetails 인터페이스를 구현한 객체
+        User principal = new User(claims.getSubject(), "", authorities);
 
-        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+        // 유저 객체, 토큰, 권한 정보들을 이용해 인증 객체를 생성해서 반환
+        return new UsernamePasswordAuthenticationToken(principal, accessToken, authorities);
     }
 
+    // 주어진 토큰의 유효성을 검사하는 메소드
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
